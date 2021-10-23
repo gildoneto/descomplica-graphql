e454369edf9794a29ee993030c4f50dd86cae598
Gildo Neto <gildoneto@hotmail.com>
2021-10-22 23:32:06 -0300

    cria database.js

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+const knexfile = require('../knexfile')
+const knex = require('knex')(knexfile)
+module.exports = knex
+
+// knex('alunos').insert({
+//     nome: 'Ena Guimaraes',
+//     cpf: '01476515426',
+//     email: 'ena.guimaraes@descomplica.com.br'
+// }).then(data => console.log(data))
+
+knex('alunos').first().then(result => console.log(result))
+// knex('alunos').then(result => console.log(result))
+// knex('alunos').select('*').then(result => console.log(result))
+// knex('alunos').where({id: 1}).then(result => console.log(result))
